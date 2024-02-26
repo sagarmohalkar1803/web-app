809db58f9a75bffbca27287f8d25144f3f5486a5
sagarmohalkar1803 <87029317+sagarmohalkar1803@users.noreply.github.com>
2024-02-26 02:28:58 +0000

    added new files

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    'id': 1,
+    'title': 'event night',
+    'eventtime': '2023-01-01',
+    'message': 'Welcome To The Best Model  Winner Contest ',
+}, {
+    'id': 2,
+    'title': 'event day',
+    'eventtime': '2023-01-02',
+    'message': 'Welcome To The Best Model  Winner Contest 12',
+}, {
+    'id': 3,
+    'title': 'event morning',
+    'eventtime': '2023-01-03',
+    'message': 'Welcome To The Best Model  Winner Contest 122',
+}, {
+    'id': 4,
+    'title': 'event mid',
+    'eventtime': '2023-01-04',
+    'message': 'Welcome To The Best Model  Winner Contest 1256',
+}, {
+    'id': 5,
+    'title': 'event new',
+    'eventtime': '2024-02-06',
+    'message': ' Contest 1256',
+    'categori': "sport"
+}]
+
 
 @app.route("/")
-def hello_world():
-  return "<p>Hello, Sagar!</p>"
+def home_page():
+  return render_template('index.html', jobs=JOBS)
 
 
-print(__name__)
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
